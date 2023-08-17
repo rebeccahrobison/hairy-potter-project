42d58fce8709ba19e746098f7ecc2402aa516c51
Rebecca Robison <rebeccahrobison@outlook.com>
2023-08-17 10:28:25 -0500

    import makePottery, create pottery objects

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,20 @@
 // Imports go first
+import { makePottery } from "./PotteryWheel.js"
 
 
 
 // Make 5 pieces of pottery at the wheel
-let mug = makePottery()
+let mug = makePottery("mug", 1, 5)
+let platter = makePottery("platter", 2, 1)
+let vase = makePottery("vase", 4, 6)
+let pot = makePottery("pot", 3, 4)
+let cup = makePottery("cup", 1, 4)
 
+console.log(mug)
+console.log(platter)
+console.log(vase)
+console.log(pot)
+console.log(cup)
 
 // Fire each piece of pottery in the kiln
 
@@ -13,6 +23,7 @@ let mug = makePottery()
 
 
 // Invoke the component function that renders the HTML list
+
 
 
 
