3d9f062ac1ccf5f4c20176a6dd883f7504ae657e
Rebecca Robison <rebeccahrobison@outlook.com>
2023-08-17 12:02:07 -0500

    import PotteryList, invoke and add to html element

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,14 +2,14 @@
 import { makePottery } from "./PotteryWheel.js"
 import { firePottery } from "./Kiln.js"
 import { toSellOrNotToSell } from "./PotteryCatalog.js"
-
+import { PotteryList } from "./PotteryList.js"
 
 // Make 5 pieces of pottery at the wheel
-let mug = makePottery("mug", 1, 5)
-let platter = makePottery("platter", 2, 1)
-let vase = makePottery("vase", 4, 6)
-let pot = makePottery("pot", 6, 4)
-let cup = makePottery("cup", 1, 4)
+let mug = makePottery("Mug", 1, 5)
+let platter = makePottery("Platter", 2, 1)
+let vase = makePottery("Vase", 4, 6)
+let pot = makePottery("Pot", 6, 4)
+let cup = makePottery("Cup", 1, 4)
 
 // console.log(mug)
 // console.log(platter)
@@ -30,15 +30,15 @@ let firedCup = firePottery(cup, 2000)
 // console.log(firedPot)
 // console.log(firedCup)
 
-// Determine which ones should be sold, and their price
-console.log(toSellOrNotToSell(firedMug))
+toSellOrNotToSell(firedMug)
 toSellOrNotToSell(firedPlatter)
 toSellOrNotToSell(firedVase)
 toSellOrNotToSell(firedPot)
 toSellOrNotToSell(firedCup)
 
 // Invoke the component function that renders the HTML list
-
-
+const createdPotteryList = PotteryList()
+const parentHTMLElement = document.querySelector(".potteryList")
+parentHTMLElement.innerHTML = createdPotteryList
 
 
