9f4be79720dbb7a7cbcc7c82e40c6075613c8193
Rebecca Robison <rebeccahrobison@outlook.com>
2023-08-17 11:27:19 -0500

    Invoke toSellOrNotToSell function on pieces

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,37 +1,41 @@
 // Imports go first
 import { makePottery } from "./PotteryWheel.js"
 import { firePottery } from "./Kiln.js"
-
+import { toSellOrNotToSell } from "./PotteryCatalog.js"
 
 
 // Make 5 pieces of pottery at the wheel
 let mug = makePottery("mug", 1, 5)
 let platter = makePottery("platter", 2, 1)
 let vase = makePottery("vase", 4, 6)
-let pot = makePottery("pot", 3, 4)
+let pot = makePottery("pot", 6, 4)
 let cup = makePottery("cup", 1, 4)
 
-console.log(mug)
-console.log(platter)
-console.log(vase)
-console.log(pot)
-console.log(cup)
+// console.log(mug)
+// console.log(platter)
+// console.log(vase)
+// console.log(pot)
+// console.log(cup)
 
 // Fire each piece of pottery in the kiln
 let firedMug = firePottery(mug, 2200)
 let firedPlatter = firePottery(platter, 1900)
 let firedVase = firePottery(vase, 2300)
-let firedPot = firePottery(pot, 2400)
+let firedPot = firePottery(pot, 2100)
 let firedCup = firePottery(cup, 2000)
 
-console.log(firedMug)
-console.log(firedPlatter)
-console.log(firedVase)
-console.log(firedPot)
-console.log(firedCup)
+// console.log(firedMug)
+// console.log(firedPlatter)
+// console.log(firedVase)
+// console.log(firedPot)
+// console.log(firedCup)
 
 // Determine which ones should be sold, and their price
-
+console.log(toSellOrNotToSell(firedMug))
+toSellOrNotToSell(firedPlatter)
+toSellOrNotToSell(firedVase)
+toSellOrNotToSell(firedPot)
+toSellOrNotToSell(firedCup)
 
 // Invoke the component function that renders the HTML list
 
